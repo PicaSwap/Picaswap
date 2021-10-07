808c9729821a194b12d609fa0f8816b756411982
Yura Kriachko <iurii.kriachko@gmail.com>
2021-10-07 23:23:10 +0200

    Implement basic interface for swapping

--- interface/src/Swap.js
+++ interface/src/Swap.js
@@ -2,9 +2,17 @@ import React, { useState, useEffect } from "react";
 
 export function Swap() {
 
+  const [tokenFrom, setTokenFrom] = useState(undefined)
+  const [tokenTo, setTokenTo] = useState(undefined)
+
   const [mode, setMode] = useState("wrap")
   const [balance, setBalance] = useState(undefined)
   const [amount, setAmount] = useState("wrap")
+
+  const coins = [
+    {name: "WCSPR", contract: "xxx"},
+    {name: "PICAS", contract: "zzz"},
+  ]
 
   async function swap(){
   }
@@ -12,11 +20,36 @@ export function Swap() {
   return (
     <div className="text-center">
       <h4>Swap</h4>
-      <div>
-            <div>Balance: {balance !== undefined ? balance : 'N/A'}</div>
-            <input placeholder="Amount" type="number" value={amount} onChange={(e) => {setAmount(parseFloat(e.target.value))}} style={{width: '200px', margin: "10px auto"}} />
+      <div className="mt-5">
+        <div className="flex justify-end mr-12">
+           From: 
+           <select className="px-4 mx-2 w-36 h-10" onChange={(e)=>{setTokenFrom(e.target.value)}} value={tokenFrom}>
+              <option value={undefined}>Select</option>
+              {coins.filter(item => item.contract && item.contract != tokenTo).map(token => <option value={token.contract}>{token.name}</option>)} 
+           </select>
+          <div className="inline-block">
+            <input type="number" placeholder="amount" className="p-2" />
+            <div className="text-right">
+            <span className="text-xs">balance: N/A</span>
+            </div>
+          </div>
+        </div>
 
-            <button onClick={swap}>{mode}</button>
+        <div className="flex justify-end mr-12 mt-8">
+           To: 
+           <select className="px-4 mx-2 w-36 h-10" onChange={(e)=>{setTokenTo(e.target.value)}} value={tokenTo}>
+              <option value={undefined}>Select</option>
+              {coins.filter(item => item.contract && item.contract != tokenFrom).map(token => <option value={token.contract}>{token.name}</option>)} 
+           </select>
+          <div className="inline-block">
+            <input type="number" placeholder="amount" className="p-2" />
+            <div className="text-right">
+            <span className="text-xs">balance: N/A</span>
+            </div>
+          </div>
+        </div>
+
+        <button className="mt-10 ml-2 py-2 px-10 border bg-green-500 text-white" onClick={swap}>Swap</button>
       </div>
     </div>
   )
